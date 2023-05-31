5662f0f12fd31e2095ede3b0d88a1d423e91d89b
somesh5368 <112803292+somesh5368@users.noreply.github.com>
2023-05-31 13:00:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Input Box cannot be blank");
+
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData()
+}
+
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
+
+function saveData(){
+        localStorage.setItem("data", listContainer.innerHTML);
+}
